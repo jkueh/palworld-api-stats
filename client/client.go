620983a8fa60b9f949b9eeaa502cdd4e8095d248
Jordan Kueh <jkueh@users.noreply.github.com>
2024-04-20 23:43:12 +0930

    Adding a wrapper around http.Client.Do() to inject auth credentials and the Content-type header

--- client/client.go
+++ client/client.go
@@ -1,6 +1,10 @@
 package palworld_api_client
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
 
 type RESTAPIClientConfig struct {
 	Host     string
@@ -10,12 +14,30 @@ type RESTAPIClientConfig struct {
 
 type RESTAPIClient struct {
 	config *RESTAPIClientConfig
+	client http.Client
 }
 
 func New(c *RESTAPIClientConfig) RESTAPIClient {
-	return RESTAPIClient{config: c}
+	return RESTAPIClient{
+		config: c,
+		client: http.Client{},
+	}
 }
 
 func (c *RESTAPIClient) DoNothing() {
 	fmt.Println("Nothing: Done")
+}
+
+// A wrapper around http.Client.Do(), so we can inject authorisation details
+func (c *RESTAPIClient) Do(method string, endpoint string) (*http.Response, error) {
+	req := http.Request{
+		Method: method,
+		URL: &url.URL{
+			User: url.UserPassword(c.config.Username, c.config.Password),
+			Host: c.config.Host,
+			Path: fmt.Sprintf("/v1/api/%s", endpoint),
+		},
+	}
+	req.Header.Add("Content-type", "application/json")
+	return c.client.Do(&req)
 }
