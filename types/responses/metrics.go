b4d4efed67b47b19ec6f24c4918e5b4932cf29f1
Jordan Kueh <jkueh@users.noreply.github.com>
2024-04-22 20:58:54 +0930

    Fixing the data type for ServerFrameTime

--- types/responses/metrics.go
+++ types/responses/metrics.go
@@ -2,9 +2,9 @@ package responses
 
 // Response from the Metrics endpoint
 type MetricsResponse struct {
-	ServerFPS        int `json:"serverfps"`
-	CurrentPlayerNum int `json:"currentplayernum"`
-	ServerFrametime  int `json:"serverframetime"`
-	MaxPlayerNum     int `json:"maxplayernum"`
-	ServerUptime     int `json:"uptime"`
+	ServerFPS        int     `json:"serverfps"`
+	CurrentPlayerNum int     `json:"currentplayernum"`
+	ServerFrameTime  float64 `json:"serverframetime"`
+	MaxPlayerNum     int     `json:"maxplayernum"`
+	ServerUptime     int     `json:"uptime"`
 }
